16ea2aef1bf2e2c3319a8337559bcfa041b6a9a4
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2023-05-01 23:49:59 +0300

    DB config

--- website-jar/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-runscript from 'website-jar/src/main/resources/sql_scripts/user.sql';
-runscript from 'website-jar/src/main/resources/sql_scripts/section.sql';
-runscript from 'website-jar/src/main/resources/sql_scripts/product.sql';
-runscript from 'website-jar/src/main/resources/sql_scripts/attribute.sql';
-runscript from 'website-jar/src/main/resources/sql_scripts/document.sql';
-runscript from 'website-jar/src/main/resources/sql_scripts/order.sql';
-runscript from 'website-jar/src/main/resources/sql_scripts/cart.sql';
